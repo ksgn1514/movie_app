55e2ad84bf6952c987f55ea46ed292b76058cb9d
kim.sg <ksgn1514@gmail.com>
2023-10-14 21:07:23 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+
+function Food({fav}) {
+  return <h3>I like {fav}</h3>;
+}
+// function Food(props) {
+//   const fav = props.fav;
+//   return <h3>I like {fav}</h3>;
+// }
 
 function App() {
   return (
     <div className="App">
-      Helloqwe
+      <h1>Hello</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+
     </div>
   );
 }
