35e96bce5f925b625f5972fdf5f9abc1e839be9f
kim.sg <ksgn1514@gmail.com>
2023-10-14 22:40:47 +0900

    proptypes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
 import React from "react";
+import PropType from "prop-types";
 
-function Food({fav}) {
-  return <h3>I like {fav}</h3>;
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
-// function Food(props) {
-//   const fav = props.fav;
-//   return <h3>I like {fav}</h3>;
-// }
+
+Food.prototype = {
+  name: PropType.string.isRequired,
+  picture: PropType.string.isRequired,
+  rating: PropType.number
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 3.9
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://m.bibigo.com/cj_mbibigo/img/kr/img_food_bibibap3.png",
+    rating: 4.8
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.5
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7
+  }
+];
+
+
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-
+      {foodILike.map(renderFood)}
     </div>
   );
 }
