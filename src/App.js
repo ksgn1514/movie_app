b6de25a2debc95603e506f8dd2d95204eb9dbc77
kim.sg <ksgn1514@gmail.com>
2023-10-15 21:49:25 +0900

    plannig the movie app

--- src/App.js
+++ src/App.js
@@ -3,37 +3,18 @@ import PropType from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true
   }
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
 
   componentDidMount() {
-    // 컴포넌트가 마운트 됐을때
-    console.log("component rendered")
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
   }
-
-  componentDidUpdate() {
-    // 컴포넌트 업데이트 됐을때 -> setStates
-    console.log("component updated")
-  }
-
-  componentWillUnmount() {
-    // 컴포넌트 죽을때
-    console.log("component unmount")
-  }
-
   render() {
-    console.log("render")
+    const { isLoading } = this.state;
     return <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>add</button>
-      <button onClick={this.minus}>minus</button>
+      {isLoading ? "Loading..." : "We are ready"}
     </div>
   }
 }
