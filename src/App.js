b326dc742c257bf15668d88336b8e8fc16001f17
kim.sg <ksgn1514@gmail.com>
2023-10-15 21:41:07 +0900

    react life cycle

--- src/App.js
+++ src/App.js
@@ -1,71 +1,41 @@
 import React from "react";
 import PropType from "prop-types";
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.prototype = {
-  name: PropType.string.isRequired,
-  picture: PropType.string.isRequired,
-  rating: PropType.number
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 3.9
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://m.bibigo.com/cj_mbibigo/img/kr/img_food_bibibap3.png",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7
+class App extends React.Component {
+  state = {
+    count: 0
   }
-];
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
 
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-}
+  componentDidMount() {
+    // 컴포넌트가 마운트 됐을때
+    console.log("component rendered")
+  }
 
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(renderFood)}
+  componentDidUpdate() {
+    // 컴포넌트 업데이트 됐을때 -> setStates
+    console.log("component updated")
+  }
+
+  componentWillUnmount() {
+    // 컴포넌트 죽을때
+    console.log("component unmount")
+  }
+
+  render() {
+    console.log("render")
+    return <div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>add</button>
+      <button onClick={this.minus}>minus</button>
     </div>
-  );
+  }
 }
 
 export default App;
